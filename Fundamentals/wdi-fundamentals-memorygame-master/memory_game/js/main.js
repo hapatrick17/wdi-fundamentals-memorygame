10c1041952b6bb207699bfa4cef84d2f9b48f9e5
hapatrick17 <45371583+hapatrick17@users.noreply.github.com>
2018-12-03 12:17:48 -0500

    Add files via upload

---
+++ Fundamentals/wdi-fundamentals-memorygame-master/memory_game/js/main.js
@@ -0,0 +1,52 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User Flipped" + cardOne);
+console.log("User Flipped" + cardThree);
+var cards = [{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"}
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",}
+	{
+	rank: "king",
+	suit: "hearts",
+	image: "images/king-of-hearts.png"}
+	{
+	rank: "king",
+	suit: "diamonds",
+	image:"images/king-of-diamonds"}
+	];
+var cardsInPlay = [];
+var setAttribute = function()
+console.log(cards[cardId].cardImage);
+var checkForMatch = function(if (cardsInPlay[0]=== cardsInPlay[1]){
+	console.log("You found a match");
+}
+else {console.log("Sorry, try again");
+}
+cardsInPlay.push(cards[0].rank);
+if (cardsInPlay.length === 2) {
+	console.log("move forward")}
+else {console.log(error);}
+console.log(cardImage);
+console.log(suit);
+var createBoard = function(for (var i=0; i<cards.length;i++{
+}
+flipcard(0);
+flipcard(2);
+var flipcard = function(this.getAttribute(cardId[data-id])
+var cardElement = function(var= 1=0; i<'data-id'; i
+var cardElement = document.createElement("images/queen-of-hearts");
+cardElement.setAttribute("images/back.png", "images/queen-of-hearts");
+var cardElement = document.createElement("images/queen-of-diamonds");
+cardElement.setAttribute("images/back.png", "images/queen-of-diamonds");
+var cardElement = document.createElement("images/king-of-hearts");
+cardElement.setAttribute("images/back.png", "images/king-of-hearts");
+var cardElement = document.createElement("imgages/king-of-diamonds");
+cardElement.setAttribute("images/back.png", "images/king-of-diamonds");
+addEventListener("click", "flip card");
